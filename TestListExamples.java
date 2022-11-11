1467fe97eadd5f70a3d59a33b4768b7be0eaa27a
AlexanderZhangWang <114326316+AlexanderZhangWang@users.noreply.github.com>
2022-11-10 17:15:55 -0800

    1

--- TestListExamples.java
+++ TestListExamples.java
@@ -3,4 +3,30 @@ import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  class ifLengthLargerThanTwo implements StringChecker{
+  public boolean checkString(String s){
+    if(s.length() > 2){
+      return true;
+    }
+    return false;
+  }
+}
+  @Test
+    public void testFilter(){
+        ArrayList<String> arr = new ArrayList<>(); arr.add("a"); arr.add("aa"); arr.add("aaa"); arr.add("aaaa");
+        ArrayList<String> arr2 = new ArrayList<>(); arr2.add("aaa"); arr2.add("aaaa");
+
+        assertEquals(arr2, ListExamples.filter(arr, new ifLengthLargerThanTwo()));
+        
+    }
+
+    @Test
+    public void testMerge(){
+        ArrayList<String> arr = new ArrayList<>(); arr.add("a"); arr.add("c"); 
+        ArrayList<String> arr2 = new ArrayList<>(); arr2.add("b"); arr2.add("d"); arr2.add("e"); arr2.add("f");
+        ArrayList<String> arr3 = new ArrayList<>(); arr3.add("a"); arr3.add("b"); arr3.add("c"); arr3.add("d");
+         arr3.add("e"); arr3.add("f");
+
+        assertEquals(arr3, ListExamples.merge(arr, arr2));
+    }
 }
