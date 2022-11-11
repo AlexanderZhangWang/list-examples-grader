0a7f84e4b557c60ec27b311d1198a6413394f24c
AlexanderZhangWang <114326316+AlexanderZhangWang@users.noreply.github.com>
2022-11-10 17:36:40 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -8,27 +8,24 @@ cp TestListExamples.java student-submission
 cd student-submission
 if[[ -e "ListExamples.java" ]]
 then 
-
+    javac ListExamples.java
+    if[[ $? -eq 0 ]]
+    then
+        echo "ListExamples.java cannot be compiled"
+        exit
+    fi
+    javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar TestListExamples.java
+    java TestListExamples.java 2> err-output.txt
+    if[[ $? -eq 0 ]]
+    then
+        echo "2/2"
+        exit
+    fi
+    echo grep -c "Error" err-output.txt "/2"
 else 
     echo "ListExamples.java not found"
     exit
 fi
 
-javac ListExamples.java
-if[[ $? -eq 0 ]]
-then
-    echo "ListExamples.java cannot be compiled"
-    exit
-fi
 
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar TestListExamples.java
-java TestListExamples.java 2> err-output.txt
-if[[ $? -eq 0 ]]
-then
-    echo "2/2"
-    exit
-fi
-
-
-echo grep -c "Error" err-output.txt "/2"
 
